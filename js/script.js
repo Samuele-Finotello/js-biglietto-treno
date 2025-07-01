fd9fa72e4dbb9dc7ab072d51426fef20666715e3
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-01 21:39:27 +0200

    [add] if < 18

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const km = parseFloat(prompt('Quanti chilometri vuoi fare?'));
 const eta = parseInt(prompt('Quanti anni hai?'));
 let basePrice = 0.21;
+
+let totalPrice = basePrice * km;
+
+if (eta < 18) {
+  let diff = totalPrice * 20 / 100;
+  totalPrice = totalPrice - diff;
+}
+
+console.log(totalPrice);
